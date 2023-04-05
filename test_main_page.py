e4ecf449f8c1bce539928e629d396ea27f5cc178
AntiPetrun <apetrunikov@tutanota.com>
2023-04-05 16:29:22 +0300

    modified pages/main_page.py(rewrited method should), pages/base_page.py(added new method) and test_main_page.py(added new test)

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,18 @@
 from selenium.webdriver.common.by import By
+from urllib3.util import url
+
 from .pages.main_page import MainPage
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+def test_should_be_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
